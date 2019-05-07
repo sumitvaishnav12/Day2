f33ac2d56d5d8de3d558db9a3f2ca3ccece39e69
surendraparmar1998 <parmarsurendra415@gmail.com>
2019-05-07 14:33:22 +0530

    commit 2

---
+++ app/src/main/java/com/example/surendra/myapplication/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.surendra.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.surendra.myapplication.R;
+
+public class MainActivity2 extends AppCompatActivity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(getIntent().getStringExtra("KEY"));
+    }
+
+}
